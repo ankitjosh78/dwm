063259e0eaa4ca30b66d2f063775ce4ab24b71c4
Ankit <ankitjosh78@gmail.com>
2021-01-07 13:56:27 +0530

    Font Small

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Medium:size=13" };
+static const char *fonts[]          = { "JetBrains Mono Medium:size=12" };
 static const char dmenufont[]       = "JetBrains Mono Medium:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
