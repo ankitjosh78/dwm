0d6471bb15bb0179fdbd3d9a89d6a94bec28fe7f
Ankit <ankitjosh78@gmail.com>
2021-01-08 16:13:50 +0530

    A few more workspace pinning

--- config.h
+++ config.h
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1⏽", "2⏽", "3⏽", "4⏽", "5⏽", "6⏽", "7⏽", "8⏽", "9⏽" };
+static const char *tags[] = { "1⏽", "2⏽", "3⏽", "4⏽ﭮ","5⏽", "6⏽", "7⏽", "8⏽", "9⏽" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -37,8 +37,10 @@ static const Rule rules[] = {
 	{ "firefox",  NULL,      	NULL,		1 << 1,       0,           -1 },
 	//{ "st-256color",  NULL,     NULL,		1 << 0,       0,           -1 },
 	//{ "Pcmanfm",  NULL, 		NULL,		1 << 2,       0,           -1 },
-	{ "discord",  NULL, 		NULL,		1 << 2,       0,           -1 },
-	{ "Steam",    NULL, 		NULL,		1 << 3,       0,           -1 },
+	{ "discord",  NULL, 		NULL,		1 << 3,       0,           -1 },
+	{ "code-oss",  NULL, 		NULL,		1 << 2,       0,           -1 },
+	{ "Steam",    NULL, 		NULL,		1 << 4,       0,           -1 },
+	{ "Lutris",    NULL, 		NULL,		1 << 4,       0,           -1 },
 	{ "Spotify",  NULL, 		NULL,		1 << 7,       0,           -1 },
 	{ "obs",  	  NULL, 	 	NULL,		1 << 8,       0,           -1 },
 	{ "SimpleScreenRecorder",  	NULL, 	 	NULL,		1 << 8,       0,           -1 },
