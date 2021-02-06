005a4f71a62f1320f8359819de939504945d60a0
Ankit <ankitjosh78@gmail.com>
2021-02-06 20:39:46 +0530

    Alacritty

--- config.def.h
+++ config.def.h
@@ -79,7 +79,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run","-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *filecmd[]  = { "pcmanfm", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "run", NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
