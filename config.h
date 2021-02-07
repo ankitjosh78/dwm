24bdd0409e9d81617a73b034a2e096eb7ffa5802
Ankit <ankitjosh78@gmail.com>
2021-02-07 22:37:11 +0530

    JoyPixels

--- config.h
+++ config.h
@@ -12,7 +12,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=12" };
+static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=12",
+                                        "JoyPixels:size=10:antialias=true:autohint=true",
+                                        "FontAwesome:size=10:antialias=true:autohint=true",};
 static const char dmenufont[]       = "JetBrains Mono Medium:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
