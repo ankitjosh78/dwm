a2289d089bfd57a1fe913c0ae7f84151180bc3db
Ankit <ankitjosh78@gmail.com>
2021-02-08 00:46:55 +0530

    Font Config

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=12:antialias=true:autohint=true",
-                                        "Dejavu Sans Mono for Powerline:size=9",
+static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=11:antialias=true:autohint=true",
+                                        "Dejavu Sans Mono for Powerline:size=10",
                                         "JoyPixels:size=10:antialias=true:autohint=true",
                                         "FontAwesome:size=10:antialias=true:autohint=true",
                                         };
--- config.h
+++ config.h
@@ -12,12 +12,12 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=12:antialias=true:autohint=true",
+static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=11:antialias=true:autohint=true",
                                         "Dejavu Sans Mono for Powerline:size=9",
                                         "JoyPixels:size=10:antialias=true:autohint=true",
                                         "FontAwesome:size=10:antialias=true:autohint=true",
                                         };
-static const char dmenufont[]           = "JetBrains Mono Nerd Font:size=12";
+static const char dmenufont[]           = "JetBrains Mono Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
