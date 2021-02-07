e82a0a56c2372c7df40c2c3ccd0e9f6cff57c143
Ankit <ankitjosh78@gmail.com>
2021-02-07 23:16:51 +0530

    Font changes, emoji support,gap size reduction, default term:alacritty

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -12,10 +12,12 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=12",
+static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=12:antialias=true:autohint=true",
+                                        "Dejavu Sans Mono for Powerline:size=9",
                                         "JoyPixels:size=10:antialias=true:autohint=true",
-                                        "FontAwesome:size=10:antialias=true:autohint=true",};
-static const char dmenufont[]       = "JetBrains Mono Medium:size=12";
+                                        "FontAwesome:size=10:antialias=true:autohint=true",
+                                        };
+static const char dmenufont[]           = "JetBrains Mono Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
