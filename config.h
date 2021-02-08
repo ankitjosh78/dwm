aa52c3f99747afdebfde56fdecec6f2ef19811c4
Ankit <ankitjosh78@gmail.com>
2021-02-08 12:29:53 +0530

    Emoji

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -13,11 +13,11 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=11:antialias=true:autohint=true",
-                                        "Dejavu Sans Mono for Powerline:size=9",
+                                        "Dejavu Sans Mono for Powerline:size=10",
                                         "JoyPixels:size=10:antialias=true:autohint=true",
                                         "FontAwesome:size=10:antialias=true:autohint=true",
                                         };
-static const char dmenufont[]           = "JetBrains Mono Nerd Font:size=11";
+static const char dmenufont[]           = "JetBrains Mono Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -83,7 +83,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run","-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *term1cmd[]  = { "alacritty", NULL };
+static const char *term2cmd[]  = { "st", NULL };
 static const char *filecmd[]  = { "pcmanfm", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "run", NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
@@ -109,8 +110,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_d, 	   spawn,          {.v = discord } },
 	{ MODKEY|ShiftMask,             XK_o, 	   spawn,          {.v = obs } },
 	{ MODKEY|ShiftMask,             XK_b, 	   spawn,          {.v = bravecmd } },
-	{ MODKEY,             			XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             			XK_Return, spawn,          {.v = term1cmd } },
 	{ ControlMask|Mod1Mask,			XK_f,	   spawn,          {.v = firefoxcmd } },
+	{ ControlMask|Mod1Mask,			XK_t,	   spawn,          {.v = term2cmd } },
 	{ MODKEY,             			XK_F12,    spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
@@ -162,7 +164,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = term1cmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
