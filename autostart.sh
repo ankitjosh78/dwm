68e5036625809b8003dab401ba2ed0f23711a81e
Ankit Josh <ankitjosh78@protonmail.com>
2021-03-14 23:08:03 +0530

    Border

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,7 @@
 
 nitrogen --restore & # wallpaper restored
 picom --experimental-backends --xrender-sync-fence & # compositor
+#.~/.config/polybar/launch.sh &
 /home/ankit/dwm-bar/dwm_bar.sh & #dwm bar
 volumeicon & #volumeicon
 
--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -12,7 +12,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=11:antialias=true:autohint=true",
+static const char *fonts[]              = {"JetBrains Mono Nerd Font:size=12:antialias=true:autohint=true",
                                         "Dejavu Sans Mono for Powerline:size=10",
                                         "JoyPixels:size=10:antialias=true:autohint=true",
                                         "FontAwesome:size=10:antialias=true:autohint=true",
@@ -22,7 +22,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#BE5046";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -56,7 +56,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
