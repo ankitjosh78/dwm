513d94810063e439941d77c6d2a0de51bc9ad076
Ankit Josh <ankitjosh78@protonmail.com>
2021-04-08 00:21:19 +0530

    autostart

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-wal -R # wallpaper restorer
 picom --experimental-backends --xrender-sync-fence & # compositor
 #.~/.config/polybar/launch.sh &
 /usr/bin/dunst & # dunst
 /home/ankit/dwm-bar/dwm_bar.sh & #dwm bar
+wal -R # wallpaper restorer
 
 
