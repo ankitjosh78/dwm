11dc13df2b55a143ee15cf18ccaf13ea27adffc1
Ankit Josh <ankitjosh78@protonmail.com>
2021-06-19 16:27:13 +0530

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,8 +2,8 @@
 
 picom --experimental-backends --xrender-sync-fence & # compositor
 #.~/.config/polybar/launch.sh &
-/usr/bin/dunst & # dunst
-/home/ankit/.local/share/dwm-bar/dwm_bar.sh & #dwm bar
+#/usr/bin/dunst & # dunst
+/home/ankit/dwm-bar/dwm_bar.sh & #dwm bar
 wal -R # wallpaper restorer
 
 
